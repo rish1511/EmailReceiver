524bcfbcc11dec664b15d0f40d9d6f29b5730e9b
rish1511 <rishabhsharma9805@gmail.com>
2026-06-05 23:29:26 +0530

    final4

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,13 @@ const caseRoutes = require("./routes/case.routes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://email-receiver-wheat.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api", caseRoutes);
